32ff96de6f75809185dc7b027cd41a5cc451f036
Edem Ablyazizov <ablyazizov.e.i20@gmail.com>
2022-04-17 22:56:54 +0300

    4.2

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,29 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* Страница Гомера Симсона*/
+router.get('/Homer_Simson', function(req, res, next) {
+  res.send("<h1>Страница Гомера Симсона</h1>")
+});
+
+/* Страница Мардж Симсона*/
+router.get('/Marge_Simson', function(req, res, next) {
+  res.send("<h1>Страница Мардж Симсона</h1>")
+});
+
+/* Страница Барта Симсона*/
+router.get('/Bart_Simson', function(req, res, next) {
+  res.send("<h1>Страница Барта Симсона</h1>")
+});
+
+/* Страница Лисы Симсона*/
+router.get('/Lisa_Simson', function(req, res, next) {
+  res.send("<h1>Страница Лисы Симсона</h1>")
+});
+
+/* Страница Мегги Симсона*/
+router.get('/Maggie_Simson', function(req, res, next) {
+  res.send("<h1>Страница Мегги Симсона</h1>")
+});
+
 module.exports = router;
