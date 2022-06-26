f470013601ce67db91983b0c42ce55ca19bcda3a
Ed_Heaven <ablyazizov.e.i20@gmail.com>
2022-06-26 13:35:33 +0300

    8.6

--- createDB.js
+++ createDB.js
@@ -1,47 +1,8 @@
-var async = require("async")
-
-var mongoose = require('mongoose')
+    var mongoose = require('mongoose')
 mongoose.connect('mongodb://localhost/simpsons-wiki')
 var Character = require("./models/character").Character
-
-function open(callback){
-    mongoose.connection.on("open",callback)
-}
-
-function dropDatabase(callback){
-    var db = mongoose.connection.db
-    db.dropDatabase(callback)
-}
-
-function createCharacters(callback){
-    async.parallel([
-            function(callback){
-                var homer = new Character({nick:"homer"})
-                homer.save(function(err,homer){
-                    callback(err,"homer")
-                })
-            },
-            function(callback){
-                var marge = new Character({nick:"marge"})
-                marge.save(function(err,marge){
-                    callback(err,"marge")
-                })
-            },
-            function(callback){
-                var bart = new Character({nick:"bart"})
-                bart.save(function(err,bart){
-                    callback(err,"bart")
-                })
-            }
-        ],
-        function(err,result){
-            callback(err)
-        })
-}
-
-function close(callback){
-    mongoose.disconnect(callback)
-}
+var async = require("async")
+var data = require('./data.js').data
 
 async.series([
     open,
@@ -53,3 +14,24 @@ function(err,result){
     if(err) throw err
     console.log("ok")
 })
+
+function open(callback){
+    mongoose.connection.on("open",callback)
+}
+
+function dropDatabase(callback){
+    var db = mongoose.connection.db
+    db.dropDatabase(callback)
+}
+
+function createCharacters(callback){
+    async.each(data, function(characterData, callback){
+        var character = new mongoose.models.Character(characterData);
+         character.save(callback);
+     },
+     callback);
+}
+
+function close(callback){
+    mongoose.disconnect(callback)
+}
