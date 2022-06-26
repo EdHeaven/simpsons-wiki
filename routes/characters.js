510a78cfa6fbea992b99244ae75f05340849e2bb
Ed_Heaven <ablyazizov.e.i20@gmail.com>
2022-06-26 15:35:23 +0300

    10.3

--- routes/characters.js
+++ routes/characters.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var Character = require("../models/character").Character
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -7,9 +8,18 @@ router.get('/', function(req, res, next) {
 });
 
 /* Страница персонажей */
-router.get("/:nick", function(req, res, next) {
-    res.send(req.params.nick);
-});
+router.get('/:nick', function(req, res, next) {
+    Character.findOne({nick:req.params.nick}, function(err,character){
+        if(err) return next(err)
+        if(!character) return next(new Error("Нет такого героя в этом мультсериале"))
+        res.render('character', {
+            title: character.title,
+            picture: character.avatar,
+            desc: character.desc
+        })
+    })
+})
+
 
 
 module.exports = router;
