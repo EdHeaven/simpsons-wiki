ebb6cae58a756a47730b841bcd18b60fbe48352f
Ed_Heaven <ablyazizov.e.i20@gmail.com>
2022-06-26 13:20:14 +0300

    8.4

--- createDB.js
+++ createDB.js
@@ -1,12 +1,41 @@
+var async = require("async")
+
 var mongoose = require('mongoose')
 mongoose.connect('mongodb://localhost/simpsons-wiki')
 var Character = require("./models/character").Character
 
-var character = new Character({
-    title: "Гомер Симпсон",
-    nick:"homer"
-})
+mongoose.connection.on("open",function(){
+    var db = mongoose.connection.db
+    db.dropDatabase(function(err){
+        if(err) throw err
 
-character.save(function(err, character, affected){
-    console.log(character.title)
+        async.parallel([
+            function(callback){
+                var homer = new Character({nick:"homer"})
+                homer.save(function(err,homer){
+                    callback(err,"homer")
+                })
+            },
+            function(callback){
+                var marge = new Character({nick:"marge"})
+                marge.save(function(err,marge){
+                    callback(err,"marge")
+                })
+            },
+            function(callback){
+                var bart = new Character({nick:"bart"})
+                bart.save(function(err,bart){
+                    callback(err,"bart")
+                })
+            }
+        ],
+        function(err,result){
+            if(err){
+                console.log(err)
+            } else {
+                console.log("Успешно созданы герои с никами: " +result.join(", "))
+            }
+            mongoose.disconnect()
+        })
+    })
 })
