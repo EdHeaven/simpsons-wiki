279f0d6960b5abbdf95e07749905efd4cf054e1d
Ed_Heaven <ablyazizov.e.i20@gmail.com>
2022-06-26 12:52:15 +0300

    8.2

--- createDB.js
+++ createDB.js
@@ -1,32 +1,15 @@
-// var MongoClient = require('mongodb').MongoClient
-// var data = require("./data.js").data
-// const uri = "mongodb://localhost:27017/"
-// const client = new MongoClient(uri)
-// async function run() {
-//     try  {
-//         await client.connect();
-//         var database = client.db("simpsons-wiki");
-//         database.dropDatabase()
-//         database = client.db("simpsons-wiki");
-//         const characters = database.collection("characters");
-//         const result = await characters.insertMany(data);
-//         console.log(`${result.insertedCount} documents were inserted`);
-//     } finally {
-//         await client.close();
-//     }
-// }
-// run()
-
 var mongoose = require('mongoose')
 mongoose.connect('mongodb://localhost/test')
 
-var Cat = mongoose.model('Cat', { name: String })
+var schema = mongoose.Schema({ name: String })
+
+schema.methods.meow = function(){
+    console.log(this.get("name") + " сказал мяу")
+}
+
+var Cat = mongoose.model('Cat', schema)
 
 var kitty = new Cat({ name: 'Пушок' })
 kitty.save(function (err) {
-    if (err) {
-        console.log(err)
-    } else {
-        console.log('Мяу')
-    }
+    kitty.meow()
 })
